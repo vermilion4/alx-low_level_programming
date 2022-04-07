f49f9792fcb62ca15648773fc693f233be90be31
Adaeze Ndupu <andupu4@gmail.com>
2022-04-07 10:35:29 -0700

    puts c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <string.h>
+
+int main(void)
+{
+	char str1[55];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+	puts(str1);
+
+	return(0);
+}
