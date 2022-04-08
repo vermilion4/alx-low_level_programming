11486a5128c36db71923b8704bdcd0359b19975a
Adaeze Ndupu <andupu4@gmail.com>
2022-04-08 16:47:05 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, c;
+
+	i = 0;
+	c = 0;
+
+	while (i <= 9)
+	{
+		c = 0;
+		while (c <= 9)
+		{
+			if (i != c && i < c)
+			{
+			putchar((i % 10) + '0');
+			putchar((c % 10) + '0');
+			if (i == 8 && c == 9)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			}
+			c++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
