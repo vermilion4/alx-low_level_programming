f3410a3c04079c66da54817529c58950de5b34e9
Adaeze Ndupu <andupu4@gmail.com>
2022-04-08 14:11:59 -0700

    0 to 9 with int and putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+
+	while (c <= 9)
+	{
+		putchar((c % 10) + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
