8aff5121fbdc0efd5feeb0102495b47a599b777e
Adaeze Ndupu <andupu4@gmail.com>
2022-04-08 17:02:02 -0700

    combination of 3 unique numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = b = c = 48;
+
+	while (a <= 57)
+	{
+		b = 48;
+		while (b <= 57)
+		{
+			c = 48;
+			while (c <= 57)
+			{
+				if (((a != b) && (b != c)) && ((a < b) && (b < c)))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == 55 && b == 56 && c == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
