26c64de6e40bbe5d43b95137a1a67c762f833dc5
Adaeze Ndupu <andupu4@gmail.com>
2022-04-12 07:29:59 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@
 
 void print_alphabet_x10(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i <= 9; i++)
-    {
-        print_alphabet();
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		void print_alphabet(void);
+	}
 }
