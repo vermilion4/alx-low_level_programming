4a933bbb2527293e2c67e9fa0b22056ce84eaca1
Adaeze Ndupu <andupu4@gmail.com>
2022-04-12 08:07:19 -0700

    check lowercase

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * main - check the code.
  *
@@ -9,11 +11,11 @@ int main(void)
 int r;
 
 r = _islower('H');
-_putchar(r + '0');
+putchar(r + '0');
 r = _islower('o');
-_putchar(r + '0');
+putchar(r + '0');
 r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
+putchar(r + '0');
+putchar('\n');
 return (0);
 }
