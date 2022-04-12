c5e7eb44a5b64f20151d9dadee1c0f32fc7204ad
Adaeze Ndupu <andupu4@gmail.com>
2022-04-12 18:04:31 +0100

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @a: parameter
+ *
+ * Return: 0
+ */
+
+int _abs(int a)
+{
+if (a >= 0)
+{
+return (a);
+}
+else
+{
+return (a * -1);
+}
+
+}
