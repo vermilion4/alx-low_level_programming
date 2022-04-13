d614e94b4f2ac79dfa361e5e38a0371e1e8ff250
Adaeze Ndupu <andupu4@gmail.com>
2022-04-13 02:54:24 +0100

    fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,21 +7,34 @@
 
 int main(void)
 {
-int i;
-long int j, k, next;
-j = 0;
-k = 1;
-for (i = 0; i < 98; i++)
+unsigned long int i, j, k, range, prev1, prev2, next1, next2;
+j = 1;
+k = 2;
+range = 1000000000;
+
+printf("%lu", j);
+for (i = 1; i < 91; i++)
 {
-next = j + k;
-j = k;
-k = next;
-if (i == 97)
+printf(", %lu", k);
+k += j;
+j = k - j;
+}
+
+prev1 = (j / range);
+prev2 = (j % range);
+next1 = (k / range);
+next2 = (k % range);
+
+for (i = 92; i < 99; ++i)
 {
-printf("%lu", next);
-break;
+printf(", %lu", next1 + (next2 / range));
+printf("%lu", next2 % range);
+next1 = next1 + prev1;
+prev1 = next1 - prev1;
+next2 = next2 + prev2;
+prev2 = next2 - prev2;
 }
-printf("%lu, ", next);
-}
+
+printf("\n");
 return (0);
 }
