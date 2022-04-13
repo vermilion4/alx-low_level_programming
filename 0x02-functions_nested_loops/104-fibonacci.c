ebe8174f81fbbc5dd6c220bf8e8247f3352e0053
Adaeze Ndupu <andupu4@gmail.com>
2022-04-13 02:08:23 +0100

    98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - first 98 fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+long int j, k, next;
+j = 0;
+k = 1;
+for (i = 0; i < 98; i++)
+{
+next = j + k;
+j = k;
+k = next;
+if (i == 97)
+{
+printf("%lu\n", next);
+break;
+}
+printf("%lu, ", next);
+}
+
+return (0);
+}
