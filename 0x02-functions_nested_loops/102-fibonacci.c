965a2352dab3ca348d4d0126da6195c9889a6ebd
Adaeze Ndupu <andupu4@gmail.com>
2022-04-13 01:48:33 +0100

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - first 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+long int j, k, next;
+j = 0;
+k = 1;
+for (i = 0; i < 50; i++)
+{
+next = j + k;
+j = k;
+k = next;
+if (i == 49)
+{
+printf("%lu", next);
+break;
+}
+printf("%lu, ", next);
+}
+return (0);
+}
