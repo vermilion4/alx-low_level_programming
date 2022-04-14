673af04bae5fec9043ed8f616e50db6466c6c85f
Adaeze Ndupu <andupu4@gmail.com>
2022-04-14 15:29:43 +0100

    void added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 
-void print_numbers()
+void print_numbers(void)
 {
 char c;
 
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 
-void print_most_numbers()
+void print_most_numbers(void)
 {
 char c;
 
