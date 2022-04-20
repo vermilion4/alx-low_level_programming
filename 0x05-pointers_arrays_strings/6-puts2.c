d505ae271227eb79b1fe9ff6813e6986d5064e70
Adaeze Ndupu <andupu4@gmail.com>
2022-04-20 21:09:44 +0100

    print every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints a string, followed by a new line, to stdout.
+ *
+ * @str: pointer
+ */
+
+void puts2(char *str)
+{
+for (; *str != '\0'; str+=2)
+{
+_putchar(*str);
+}
+_putchar('\n');
+}
