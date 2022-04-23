128e102a2ec7d7f8cc3b938d17fdb2b18917013f
Adaeze Ndupu <andupu4@gmail.com>
2022-04-23 21:06:13 +0100

    leet encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - encode string
+ *
+ * @s: string param
+ * Return: char*
+ */
+
+
+char *leet(char *s)
+{
+int i, j;
+i = 0;
+int arr[8] = {'o', 'l', 0, 'e', 'a', 0, 0, 't'};
+
+while (s[i])
+{
+j = 0;
+while (j < 8)
+{
+if (s[i] == arr[j] || s[i] - 'A' + 'a' == arr[j])
+{
+s[i] = j + 48;
+}
+j++;
+}
+i++;
+}
+
+return (s);
+}
