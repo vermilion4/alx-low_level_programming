bd7120a5416e37d8083b89cfb8cdb84ec1f47584
Adaeze Ndupu <andupu4@gmail.com>
2022-04-24 20:45:26 +0100

    rot13 encryption

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,19 +9,12 @@
 
 char *rot13(char *s)
 {
-char alpha[53] = {'a', 'b', 'c', 'd', 'e', 'f',
-'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-'U', 'V', 'W', 'X', 'Y', 'Z'};
+char alpha[53] =
+"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-char rot13[53] = {'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c',
-'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-'H', 'I', 'J', 'K', 'L', 'M'};
+char rot13[53] =
+"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 int i, j;
 
 i = 0;
