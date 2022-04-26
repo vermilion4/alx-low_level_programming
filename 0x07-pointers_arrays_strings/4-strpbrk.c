6d24b0e371384f3399614e2e4319e6ac5d26131f
Adaeze Ndupu <andupu4@gmail.com>
2022-04-26 22:25:33 +0100

    condition reworded

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,10 +12,10 @@ char *_strpbrk(char *s, char *accept)
 int i = 0, j;
 char *p;
 
-while (s[i] != '\0')
+while (s[i])
 {
 j = 0;
-while (accept[j] != '\0')
+while (accept[j])
 {
 if (s[i] == accept[j])
 {
