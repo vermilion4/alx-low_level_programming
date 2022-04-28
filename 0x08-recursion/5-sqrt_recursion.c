a266545a8034bd52b1b509a982ff258bcd639a7a
Adaeze Ndupu <andupu4@gmail.com>
2022-04-28 17:42:58 +0100

    square root recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of a number.
+ * @n: number
+ * Return: factorial of n
+ */
+
+int natural_sqrt(int n, int min, int max);
+
+int _sqrt_recursion(int n)
+{
+natural_sqrt(n, 1, n);
+}
+
+/* Find natural square root */
+int natural_sqrt(int n, int min, int max)
+{
+int i = (min + max) / 2;
+
+if (n < 0)
+return (-1);
+
+if (i * i == n)
+return (i);
+
+if (min == max)
+return (-1);
+
+else if (i * i < n)
+natural_sqrt(n, i + 1, max);
+
+else
+natural_sqrt(n, min, i - 1);
+
+}
