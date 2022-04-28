a54f820d42816bfafa532a75dbfb9c04384c3234
Adaeze Ndupu <andupu4@gmail.com>
2022-04-28 15:52:02 +0100

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string in reverse with recursion.
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+if (*s != '\0')
+_print_rev_recursion(s + 1);
+
+_putchar(*s);
+}
