a2916d85383a2e619088ec991a0ad22008bc91f4
Adaeze Ndupu <andupu4@gmail.com>
2022-05-02 21:20:09 +0100

    removed condition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,8 +14,6 @@ int main(int argc, char **argv)
 {
 int i, sum = 0;
 
-if (argc >= 3)
-{
 for (i = 1; i < argc; i++)
 {
 if (atoi(argv[i]))
@@ -31,6 +29,4 @@ return (1);
 printf("%d\n", sum);
 return (0);
 }
-printf("%d\n", 0);
-return (0);
-}
+
