d910d39df54ad692687cc66e901f3dc914d94d86
Adaeze Ndupu <andupu4@gmail.com>
2022-05-02 16:28:05 +0100

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - entry point
@@ -9,8 +9,8 @@
  * Return: 0
  */
 
-int main(__attribute__((unused)) int argc, char* argv[])
+int main(__attribute__((unused)) int argc, char **argv)
 {
-printf("%s", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
