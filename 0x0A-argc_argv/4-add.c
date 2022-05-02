ccc4204ab6383c58232ee985583f6dffeae6e5bb
Adaeze Ndupu <andupu4@gmail.com>
2022-05-02 22:04:15 +0100

    method change

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,23 +13,23 @@
 
 int main(int argc, char **argv)
 {
-int i, sum = 0;
+int i = 1, j, sum = 0;
 
-
-for (i = 1; i < argc; i++)
+while(i < argc)
 {
-if (*argv[i] != '\0' && atoi(argv[i]))
+j = 0;
+while (argv[i][j] != '\0')
 {
-sum += atoi(argv[i]);
-}
-else
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
 }
+j++;
 }
-
+sum += atoi(argv[i]);
+i++;
+}
 printf("%d\n", sum);
 return (0);
 }
-
