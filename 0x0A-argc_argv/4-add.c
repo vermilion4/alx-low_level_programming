8486ff32a127c697f5f321e65fe74b8dc4dd5abf
Adaeze Ndupu <andupu4@gmail.com>
2022-05-02 20:57:52 +0100

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point, add positive numbers
+ *
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 0 if added, 1 otherwise
+ */
+
+int main(int argc, char **argv)
+{
+int i, sum = 0;
+
+if (argc >= 2)
+{
+for (i = 1; i < argc; i++)
+{
+if (atoi(argv[i]))
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", sum);
+return (0);
+}
+printf("%d\n", 0);
+return (1);
+}
