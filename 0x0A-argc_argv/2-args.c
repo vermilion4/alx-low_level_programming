a2923601cbefe95338e729da8a8fc9295a735fb8
Adaeze Ndupu <andupu4@gmail.com>
2022-05-02 16:43:06 +0100

    print  all arrguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point, print arguments
+ *
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+int i = 0;
+while (i < argc)
+printf("%s", argv[i]);
+return (0);
+}
