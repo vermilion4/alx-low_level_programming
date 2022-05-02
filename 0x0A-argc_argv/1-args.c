3220c049edf1d50cfd17cd73cdbca42760ac5cf9
Adaeze Ndupu <andupu4@gmail.com>
2022-05-02 16:31:23 +0100

    print arguments number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point, print number of arguments
+ *
+ * @argv: argument vector (unused)
+ * @argc: argument count 
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+printf("%d\n", argc);
+return (0);
+}
