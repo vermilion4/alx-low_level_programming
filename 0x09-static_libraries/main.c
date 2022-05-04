c3a076e3c8927fb674f615c6b7e22602037350ae
Adaeze Ndupu <andupu4@gmail.com>
2022-05-04 19:35:37 +0100

    main test

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,18 @@
 
 int main(void)
 {
+    char *str;
+    int len;
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+
     return (0);
 }
