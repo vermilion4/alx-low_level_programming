c1d11d0335a2e43b53f1c9799aae7eba7b1d5faa
Adaeze Ndupu <andupu4@gmail.com>
2022-05-10 00:16:27 +0100

    prototype semi colon

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,7 +8,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
-int _isdigit(char *s)
+int _isdigit(char *s);
 void error(void);
 
 #endif
