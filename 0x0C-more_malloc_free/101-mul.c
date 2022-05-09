d66768b87a77cc9f09e9ef74c94d5ad97c5fedb9
Adaeze Ndupu <andupu4@gmail.com>
2022-05-09 23:55:57 +0100

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,12 +12,10 @@
 int _strlen(char *s)
 {
 int count = 0;
-
 for (; *s != '\0'; s++)
 {
 count++;
 }
-
 return (count);
 }
 
@@ -49,18 +47,15 @@ int main(int argc, char *argv[])
 {
 char *p1, *p2;
 int i, *mul, len, len1, len2, first, second, remainder, a = 0;
-
 p1 = argv[1], p2 = argv[2];
 if (argc != 3 || !_isdigit(p1) || !_isdigit(p2))
 {
 printf("Error\n");
 exit(98);
 }
-
 len1 = _strlen(p1), len2 = _strlen(p2);
 len = len1 + len2 + 1;
 mul = malloc(len *sizeof(*mul));
-
 if (mul == NULL)
 return (1);
 for (i = 0; i <= len1 + len2; i++)
@@ -72,7 +67,7 @@ remainder = 0;
 for (len2 = _strlen(p2) - 1; len2 >= 0; len2--)
 {
 second = p2[len2] - '0';
-remainder += mul[len1 + len2 + 1] + (first * second);
+remainder += mul[len1 + len2 + 1] + (first *second);
 mul[len1 + len2 + 1] = remainder % 10;
 remainder /= 10;
 }
