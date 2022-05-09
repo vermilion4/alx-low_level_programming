e82139fed4d6da7b1745ac40745b2811a369021c
Adaeze Ndupu <andupu4@gmail.com>
2022-05-09 23:11:00 +0100

    multiply test

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _realloc - function that reallocates a memory block using malloc and free.
+ *
+ * @ptr: pointer to old size param
+ * @old_size: old size param
+ * @new_size: new size param
+ * Return: pointer.
+ */
+
+int main(int argc, char *argv[])
+{
+unsigned int a, b, mul;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+mul = a * b;
+
+printf("%d", mul);
+return (0);
+}
