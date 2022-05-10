38cac9b61198436dab5ed0f2bbd9d78310e903c4
Adaeze Ndupu <andupu4@gmail.com>
2022-05-10 06:06:34 +0100

    test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,10 +15,10 @@ int i = 0;
 while (s[i])
 {
 if (s[i] < '0' || s[i] > '9')
-return (1);
+return (0);
 i++;
 }
-return (0);
+return (1);
 }
 
 /**
@@ -60,7 +60,7 @@ char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 s1 = argv[1], s2 = argv[2];
-if (argc != 3 || is_digit(s1) == 1 || is_digit(s2) == 1)
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
 len1 = _strlen(s1);
 len2 = _strlen(s2);
