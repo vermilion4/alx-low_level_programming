e8a7e7502826ea1501343c28838dd36e760eb020
Adaeze Ndupu <andupu4@gmail.com>
2022-05-11 22:50:21 +0100

    initialize dog function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a dog
+ * @d: pointer to the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner 
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
+}
