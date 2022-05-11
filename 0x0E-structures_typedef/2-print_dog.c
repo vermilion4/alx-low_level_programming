32f29d7bc7e0871122940bd8baade74ddd394453
Adaeze Ndupu <andupu4@gmail.com>
2022-05-11 23:25:01 +0100

    age null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,5 +17,8 @@ printf("Name: (nil)\n");
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 
+if (d->age == NULL)
+printf("Age: (nil)\n");
+
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
