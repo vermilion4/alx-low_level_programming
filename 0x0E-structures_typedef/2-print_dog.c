364ac3edf312229bf9fef9e726a122a3c9208569
Adaeze Ndupu <andupu4@gmail.com>
2022-05-11 23:30:08 +0100

    null fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@ if (d == NULL)
 return;
 
 if (d->name == NULL)
-d->name = "";
+d->name = "(nil)";
 
 if (d->owner == NULL)
-d->owner = "";
+d->owner = "(nil)";
 
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
