8f0109879ef926c25ab467a9750e1d5884169c3d
Adaeze Ndupu <andupu4@gmail.com>
2022-05-11 23:47:33 +0100

    str copy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 
 /**
+ * *_strcpy - copies string from pointer to pointer.
+ *
+ * @dest: destination
+ * @src: source
+ * Return: pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+int i = -1;
+do {
+i++;
+dest[i] = src[i];
+} while (src[i] != '\0');
+
+return (dest);
+}
+
+/**
  * new_dog - initialize a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -29,6 +49,9 @@ if (d->owner == NULL)
 free(d);
 return (NULL);
 }
+
+_strcpy(d->name, name);
+_strcpy(d->owner, owner);
 
 return (d);
 }
