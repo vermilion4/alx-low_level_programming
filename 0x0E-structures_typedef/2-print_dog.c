423583115b20f022983b5a619bd1f09db1718fd9
Adaeze Ndupu <andupu4@gmail.com>
2022-05-11 23:26:57 +0100

    nil change

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,13 +12,10 @@ if (d == NULL)
 return;
 
 if (d->name == NULL)
-printf("Name: (nil)\n");
+d->name = "";
 
 if (d->owner == NULL)
-printf("Owner: (nil)\n");
-
-if (d->age == 0.0)
-printf("Age: (nil)\n");
+d->owner = "";
 
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
