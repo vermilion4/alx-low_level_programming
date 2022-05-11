8528e2f4ed108e0d207b719622863c02f241957a
Adaeze Ndupu <andupu4@gmail.com>
2022-05-11 23:49:06 +0100

    new dog fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,26 +2,6 @@
 #include <stdlib.h>
 
 /**
- * *_strcpy - copies string from pointer to pointer.
- *
- * @dest: destination
- * @src: source
- * Return: pointer
- */
-
-char *_strcpy(char *dest, char *src)
-{
-
-int i = -1;
-do {
-i++;
-dest[i] = src[i];
-} while (src[i] != '\0');
-
-return (dest);
-}
-
-/**
  * new_dog - initialize a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -47,11 +27,10 @@ d->owner = owner;
 if (d->owner == NULL)
 {
 free(d);
+free(d->name);
 return (NULL);
 }
 
-_strcpy(d->name, name);
-_strcpy(d->owner, owner);
 
 return (d);
 }
