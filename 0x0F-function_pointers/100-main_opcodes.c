1fa7e55dbe38549b4ca729fdbabcb161bd6a86ed
Adaeze Ndupu <andupu4@gmail.com>
2022-05-13 22:38:35 +0100

    prints opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of an argument passed to argv
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ * (the opcodes of the program)
+ */
+
+int main(int argc, char *argv[])
+{
+int i, num;
+int (*f)(int, char **) = main;
+unsigned char *opcodes = (unsigned char *)f;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+num = atoi(argv[1]);
+
+if (num < 0)
+{
+printf("Error\n");
+exit(1);
+}
+
+for (i = 0; i < num; i++)
+{
+printf("%02x", opcodes[i]);
+
+if (i == num - 1)
+continue;
+printf(" ");
+}
+printf("\n");
+return (0);
+}
+
+
