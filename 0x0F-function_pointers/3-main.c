ea3f6b87dcebf9677980ae465aa1ddc1e3b74382
Adaeze Ndupu <andupu4@gmail.com>
2022-05-13 21:47:56 +0100

    main fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,30 +10,24 @@
  * */
 int main(int argc, char *argv[])
 {
-int (*op)(int, int);
 int num1, num2, res;
+char *op;
 
-if (argc != 4)
-{
-printf("Error\n");
-exit (98);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-op = get_op_func(argv[2]);
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    op = argv[2];
 
-if ((num2 == 0 && op == op_div) || (num2 == 0 && op == op_mod))
-{
-printf("Error\n");
-exit (100);
-}
-
-if (op == NULL)
-{
-printf("Error\n");
-exit (99);
-}
-res = op(num1, num2);
-printf("%d\n", res);
-return (0);
+    if (get_op_func(op) == NULL || op[1] != '\0')
+    {
+        printf("Error\n");
+        exit(99);
+    }
+    res = (*get_op_func(op))(num1, num2);
+    printf("%d\n", res);
+    return (0);
 }
