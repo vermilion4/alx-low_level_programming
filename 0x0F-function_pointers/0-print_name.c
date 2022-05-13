a5bcc4af3f74758b94924a1f9391f22c87976629
Adaeze Ndupu <andupu4@gmail.com>
2022-05-13 20:38:34 +0100

    betty fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: function to print the name
+ * Return: Nothing.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 f(name);
