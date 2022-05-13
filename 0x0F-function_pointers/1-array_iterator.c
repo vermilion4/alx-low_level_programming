d927416ef1e1c44a6b329cdd26a633ff3672218d
Adaeze Ndupu <andupu4@gmail.com>
2022-05-13 20:54:43 +0100

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: the integer to print
+ * @size: the size of the array
+ * @action: the function to print the integer
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+
+if (array && action)
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}
