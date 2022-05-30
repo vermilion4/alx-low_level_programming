b6cd8f0df52dbdfaa0c454110b296b6d7ff1e41a
Adaeze Ndupu <andupu4@gmail.com>
2022-05-31 00:59:01 +0100

    fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,19 +8,25 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
+const listint_t *current, *next;
 size_t i = 0;
-long int temp;
 
-while (head)
+if (head == NULL)
+return (0);
+
+current = head;
+next = head->next;
+
+while (next != NULL && next < current)
 {
-temp = head - head->next;
-i++;
-printf("[%p] %d\n", (void *)head, head->n);
-if (temp < 0)
-{
-printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+i += 1;
+printf("[%p] %d\n", (void *)current, current->n);
+current = current->next;
+next = next->next;
 }
-head = head->next;
-}
+printf("[%p] %d\n", (void *)current, current->n);
+i += 1;
+if (next != NULL)
+printf("-> [%p] %d\n", (void *)next, next->n);
 return (i);
 }
