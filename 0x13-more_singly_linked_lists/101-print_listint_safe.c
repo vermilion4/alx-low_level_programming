4d6289aa6cb94b396a79cb38fc4610c489386ba4
Adaeze Ndupu <andupu4@gmail.com>
2022-05-31 00:10:47 +0100

    betty fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_listint_safe - prints a listint_t list
+ * @head: pointer to the first element of the list
+ * Return: number of nodes
+ */
+
 size_t print_listint_safe(const listint_t *head)
 {
 size_t i = 0;
