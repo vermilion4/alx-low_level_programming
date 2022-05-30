947913e9760b6b7e490a2a16866a8182ad65f33f
Adaeze Ndupu <andupu4@gmail.com>
2022-05-31 00:09:52 +0100

    print (safe v)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+size_t print_listint_safe(const listint_t *head)
+{
+size_t i = 0;
+long int temp;
+
+while (head)
+{
+temp = head - head->next;
+i++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (temp <= 0)
+{
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+break;
+}
+else
+{
+head = head->next;
+}
+}
+return (i);
+}
