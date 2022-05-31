87878bd87ed1c56b8232c8327cacc550cb6f8e13
Adaeze Ndupu <andupu4@gmail.com>
2022-05-31 02:25:21 +0100

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,8 +12,13 @@ int get_bit(unsigned long int n, unsigned int index)
 int mask;
 int bit;
 
+if (index > sizeof(unsigned long int) * 8)
+{
+return (-1);
+}
 mask = 1 << index;
 bit = n & mask;
 bit = bit >> index;
 return (bit);
+
 }
