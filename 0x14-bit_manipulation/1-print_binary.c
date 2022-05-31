dbd12e4a7d81ed2ed6a3484f78514578fc66a1dd
Adaeze Ndupu <andupu4@gmail.com>
2022-05-31 02:15:55 +0100

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,16 +8,10 @@
 
 void print_binary(unsigned long int n)
 {
-unsigned long int mask;
-
-mask = 1 << (sizeof(unsigned long int) * 8 - 1);
-
-while (mask > 0)
+if (n > 1)
 {
-if (n & mask)
-_putchar('1');
-else
-_putchar('0');
-mask = mask >> 1;
+print_binary(n / 2);
 }
+_putchar((n % 2) + '0');
+
 }
