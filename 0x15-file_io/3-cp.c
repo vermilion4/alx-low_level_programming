e412096ed96ad6174ccbd2ebe0c80443252ce2fb
Adaeze Ndupu <andupu4@gmail.com>
2022-06-02 04:39:12 +0100

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+int main(int argc, char **argv)
+{
+int file1, file2, n;
+char buf[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+file1 = open(argv[1], O_RDONLY);
+if (file1 == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (file2 == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+
+while ((n = read(file1, buf, 1024)) > 0)
+{
+if (write(file2, buf, n) != n)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+}
+
+if (n == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+close(file1);
+close(file2);
+
+return (0);
+}
