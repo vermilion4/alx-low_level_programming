c45bc18714b60abbd3697c93e7b8fde29d10b6a2
Adaeze Ndupu <andupu4@gmail.com>
2022-06-02 04:41:08 +0100

    betty fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: the actual number of letters read
+ */
 
 int main(int argc, char **argv)
 {
