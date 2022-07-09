bfbf3972c8d8cb23a3b1f298d0cdb36fc0614b88
Adaeze Ndupu <andupu4@gmail.com>
2022-07-09 12:52:51 +0100

    space removed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,13 +7,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i;
+size_t i;
 
-    i = 0;
-    while (h != NULL)
-    {
-        i++;
-        h = h->next;
-    }
-    return (i);
+i = 0;
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
 }
